309109adb39596a59852a6a6f291b6006fd57e73
Sandeep7515 <padalasandeep7515@gmail.com>
2026-07-08 23:53:53 +0530

    Added recursion examples

---
+++ learn basic recursion/Recursion_Ex.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Recursion_Ex
+{
+    static void print_num(int n) 
+    {
+        if (n == 0)
+        {
+            return;
+        }
+        System.out.print(n + " ");
+        print_num(n - 1);
+    }
+
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number : ");
+        int n = sc.nextInt();
+        System.out.print("It prints from "+ n + " to  1 : " );
+        print_num(n);
+    }
+
+}
